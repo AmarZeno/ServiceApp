bff576a695e7b015df28cb5bdc79d79bde20b208
Zeno Saviour <amarzeno@outlook.com>
2020-04-12 23:41:13 -0400

    Added find case details

---
+++ ServiceApp/Assets/Insurance App/assets/Scripts/AWSManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Amazon;
+using UnityEngine;
+
+namespace ServiceApp
+{
+	public class AWSManager : MonoBehaviour
+	{
+		#region Monobehavior Methods
+		private void Awake()
+		{
+			UnityInitializer.AttachToGameObject(this.gameObject);
+		}
+		#endregion
+	}
+}
